0ddb60190421f1b97dce10d279b8ae3c15eea227
jeremyytann <tanchiachun2021@gmail.com>
2022-10-28 03:15:21 +0800

    modified button color and complete resume page

--- src/Pages/ResumePage.js
+++ src/Pages/ResumePage.js
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../Components/Navbars/Navbar'
+import { getStorage, ref, getDownloadURL } from "firebase/storage";
+import { app } from '../firebase-config';
+import { SpecialZoomLevel, Viewer, Worker } from '@react-pdf-viewer/core';
 
 const ResumePage = () => {
+    const storage = getStorage(app);
+    const [resume, setResume] = useState(null);
+
+    useEffect(()=>{
+        getDownloadURL(ref(storage, 'Resume/resume.pdf')).then((url)=>{
+            setResume(url);
+        })
+    }, [storage]);
+
     return (
         <div>
             <Navbar />
 
-            ResumePage
+            { resume !== null && (
+                <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.15.349/build/pdf.worker.min.js">
+                    <Viewer fileUrl={resume} defaultScale={SpecialZoomLevel.PageFit} />
+                </Worker>
+            )}
         </div>
     )
 }
